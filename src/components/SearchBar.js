23e5341ccc4623c9e9756a6bdbb3114e52238800
afrisian21 <doppong791@outlook.com>
2018-11-17 00:50:01 -0500

    Search Button Invokes Function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 class SearchBar extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        value: ''
+      };
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+     this.setState({value: event.target.value});
+   }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    console.log("Inside Function");
+    event.preventDefault();
+  }
+
   render() {
     return (
-
-      <Form>
+      <Form onSubmit={this.handleSubmit}>
         <FormGroup>
           <Label for="exampleEmail">Enter Zip Code: </Label>
-          <Input type="text" name="ZipInput" id="Zip" placeholder=" " />
+          <Input type="text" name="ZipInput" id="Zip" value={this.state.value} onChange={this.handleChange} />
         </FormGroup>
-        <Button>Submit</Button>
+        <Button >Submit</Button>
       </Form>
 
     );
